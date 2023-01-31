f2a56eae0bca896733181622a71d650f8f34a781
Tommy <tommy.alfarabi@gmail.com>
2023-01-31 21:37:02 +0700

    penambahan head component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
+import Head from "next/head";
 import Link from "next/link";
 
 function HomePage() {
     console.log('[HomePage] render');
     return (
         <>
+            <Head>
+                <title>My Blog</title>
+                <meta name="description" value="This is my Blog" />
+            </Head>
             <main>
                 <h1>My Blog</h1>
             </main>
