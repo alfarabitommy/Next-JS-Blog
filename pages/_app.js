43837efefa7af27cef420c4ab27a3fa89d02c58d
Tommy <tommy.alfarabi@gmail.com>
2023-02-01 10:49:49 +0700

    penambahan icon dan pembuatan folder assets

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import NavBar from "../components/NavBar";
 import '../styles/globals.css';
+import Head from "next/head";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/icons/favicon.ico" />
+      </Head>
       <header>
         <NavBar />
       </header>
