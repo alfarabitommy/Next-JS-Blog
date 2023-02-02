7df1d3ed8175a7bc983dc7178e7fbc434d9dcc0d
Tommy <tommy.alfarabi@gmail.com>
2023-02-02 14:11:47 +0700

    penambahan supressHydrationWarning untuk menghilangkan error saat terjadi perbedaan antara klien dan server

--- components/ThemeSwitch.js
+++ components/ThemeSwitch.js
@@ -1,16 +1,31 @@
 import { useState } from "react";
 import DarkTheme from "./DarkTheme";
 
+function loadDarkMode() {
+    if (typeof localStorage === 'undefined') {
+        return false;
+    }
+    const value = localStorage.getItem('darkMode');
+    return (value === null) ? false : JSON.parse(value);
+}
+
 // ini adalah contoh penggunaan button untuk merubah tema website kita
 // misalnya dari light mode menjadi dark mode maupun sebaliknya
 function ThemeSwitch() {
-    const [darkMode, setDarkMode] = useState(false);
+    const [darkMode, setDarkMode] = useState(loadDarkMode);
+
+    // ini adalah constant dan pemyimpanan sementara pilihan user untuk disimpan di dalam local storage
+    const handleClick = () => {
+        localStorage.setItem('darkMode', JSON.stringify(!darkMode));
+        setDarkMode(!darkMode);
+    };
 
     console.log('[ThemeSwitch] darkMode:', darkMode);
     const text = darkMode ? 'Light Mode' : 'Dark Mode';
     return (
         <>
-            <button onClick={() => setDarkMode(!darkMode)}>
+            {/* supressHydrationWarning berfungsi untuk menghilangkan error hydration akibat adanya perbedaan pada client dan server */}
+            <button onClick={handleClick} suppressHydrationWarning>
                 {text}
             </button>
             <style jsx>{`
