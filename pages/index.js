fe0aeace123794e11020e45b1575b1ec8731d4dd
Tommy <tommy.alfarabi@gmail.com>
2023-02-02 10:27:15 +0700

    memasukkan listing posts di dalam homepage

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
 import Head from "next/head";
 import Link from "next/link";
+import { getPosts } from "../lib/posts";
 
-function HomePage() {
-    console.log('[HomePage] render');
+// menaruh listing post secara dinamis, di inisiasi dari file lib/posts dengan function getPosts
+export async function getStaticProps() {
+    const posts = await getPosts();
+    return {
+        props: { posts },
+    };
+}
+
+function HomePage({ posts }) {
+    console.log('[HomePage] render', posts);
     return (
         <>
             <Head>
@@ -12,11 +21,14 @@ function HomePage() {
             <main>
                 <h1>My Blog</h1>
                 <ul>
-                    <li>
-                        <Link href="/posts/first-post">
-                            First Post
-                        </Link>
-                    </li>
+                    {/* menampilkan per masing masing post yang di parsing dari function getStaticProps diatas, reminder bahwa untuk setiap foreach react harus ditampilkan 'key' yang berisi unique value dari per masing masing list */}
+                    {posts.map((post) => (
+                        <li key={post.slug}>
+                            <Link href={`/posts/${post.slug}`}>
+                                {post.title}
+                            </Link>
+                        </li>
+                    ))}
                 </ul>
             </main>
         </>
