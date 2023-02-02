df3fbc941dadebe7f1cf6404b65eba2042bbcc30
Tommy <tommy.alfarabi@gmail.com>
2023-02-02 10:51:26 +0700

    menambahkan themeswitch component yang berguna untuk mengganti tema halaman menjadi dark mode maupun light mode

---
+++ components/ThemeSwitch.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+// ini adalah contoh penggunaan button untuk merubah tema website kita
+// misalnya dari light mode menjadi dark mode maupun sebaliknya
+function ThemeSwitch() {
+    const [darkMode, setDarkMode] = useState(false);
+
+    console.log('[ThemeSwitch] darkMode:', darkMode);
+    const text = darkMode ? 'Light Mode' : 'Dark Mode';
+    return (
+        <>
+            <button onClick={() => setDarkMode(!darkMode)}>
+                {text}
+            </button>
+            <style jsx>{`
+             button {
+                background: none;
+                border: none;
+                cursor: pointer;
+             }    
+             `}</style>
+        </>
+    );
+}
+
+export default ThemeSwitch;
